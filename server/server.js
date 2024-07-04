ae6827c8936290252199c2c54cfacc888433b112
Imadh-Ifham <imadhifham@gmail.com>
2024-07-04 12:54:53 +0530

    1 - auth controllers added

--- server/server.js
+++ server/server.js
@@ -2,12 +2,16 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000; //5000
 
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse from data(urlencoded)
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
