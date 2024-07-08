31fd35feb02f3f8910104993f7e9041bd2b89208
Imadh-Ifham <imadhifham@gmail.com>
2024-07-08 23:31:59 +0530

    4 - prepared our app for deployment

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -20,14 +21,24 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000; //5000
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse from data(urlencoded)
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
